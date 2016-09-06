69a61b2db5372be3a3eca7701e07b147679b3bb1
vitalysacred <vitalysacred@gmail.com>
2016-09-07 01:02:11 +0700

    first commit

---
+++ miraclepills/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  miraclepills
+//
+//  Created by Vitaly Sacred on 07/09/16.
+//  Copyright © 2016 Vitaly Sacred. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // self.view.backgroundColor = UIColor.purple
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
